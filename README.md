68d62529ae37de1461c34d5fcfa9efff96549375
sliver <slivertea@gmail.com>
2024-04-17 09:39:52 -0700

    .

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# axisO365App
+# Axis O365 Dynamic Network Range Application
+
+
+** Application for dynamic O365 configuration from
+** Microsoft
+
+
+## Sources
+* https://docs.axissecurity.com
+* Document reference https://learn.microsoft.com/en-us/microsoft-365/enterprise/urls-and-ip-address-ranges?view=o365-worldwide
+* JSON source: https://endpoints.office.com/endpoints/worldwide?clientrequestid=b10c5ed1-bad1-445f-b386-b919946339a7
+
+
+
+## data
+* Samples included - a json o365 json sample, and manual bulk import to Axis.
---
+++ o365GetSource.py
@@ -0,0 +1,25 @@
+import requests
+import json
+import os
+import csv
+
+#Define static values
+TOKEN = os.getenv('AXIS_TOKEN')
+axisUrl = "https://admin-api.axissecurity.com/api/v1.0/applications?pageSize=100&pageNumber=1"
+o365Url = "https://endpoints.office.com/endpoints/worldwide?clientrequestid=b10c5ed1-bad1-445f-b386-b919946339a7"
+localo365file = "sample_o365Source.json"
+apiToken = 'Bearer '+TOKEN
+payload = ""
+headers = {
+    'Authorization': apiToken,
+    'Content-Type': 'application/json'
+}
+
+
+
+#STEP1 - Collect existing o365 data
+o365Source = (requests.request("GET", o365Url, headers=headers, data=payload).json())
+# Dump output to the sample file
+with open(localo365file, 'w', encoding='utf-8') as fh:
+    json.dump(o365Source, fh, ensure_ascii=False, indent=4)
+#print (json.dumps(o365Source))
