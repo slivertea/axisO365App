56b556068f927d8013aa3c800ddd8d0c6599cd2e
sliver <slivertea@gmail.com>
2024-04-24 11:18:19 -0700

    .

--- o365.py
+++ o365.py
@@ -8,7 +8,9 @@ TOKEN = os.getenv('AXIS_TOKEN')
 axisUrl = "https://admin-api.axissecurity.com/api/v1.0/applications?pageSize=100&pageNumber=1"
 o365Url = "https://endpoints.office.com/endpoints/worldwide?clientrequestid=b10c5ed1-bad1-445f-b386-b919946339a7"
 localo365file = "sample_o365Source.json"
-csvParseFile='outputCSVparse.csv'
+inputTemplateCSV = "axisImport_NR.csv"
+outputAppsByDestination='outputAppsByDestination.csv'
+outputFileJS='outputAppsByName.json'
 apiToken = 'Bearer '+TOKEN
 payload = ""
 headers = {
@@ -29,15 +31,15 @@ headers = {
 with open(localo365file, 'r', encoding='utf-8') as fh:
     o365Source = json.load(fh)
 
-
-apps = []
+# Breakdown the source data by destination, one row per destination
+appsByDestination = []
 for o365Id in o365Source:
     #print (o365Id['serviceAreaDisplayName'], o365Id.keys())
     # Destinations
     # URL
     if "urls" in o365Id.keys():
         for url in o365Id['urls']:
-            app = {
+            appByDest= {
                 "id": o365Id['id'],
                 "category": o365Id['category'],
                 "name": o365Id['serviceAreaDisplayName'],
@@ -48,19 +50,19 @@ for o365Id in o365Source:
                 "notes": None
             }
             if "tcpPorts" in o365Id.keys():
-                app['tcpPorts'] = o365Id['tcpPorts']
+                appByDest['tcpPorts'] = o365Id['tcpPorts']
             if "udpPorts" in o365Id.keys():
-                app['udpPorts'] = o365Id['udpPorts']
+                appByDest['udpPorts'] = o365Id['udpPorts']
             if "notes" in o365Id.keys():
-                app['notes'] = o365Id['notes']
-            apps.append(app)
+                appByDest['notes'] = o365Id['notes']
+            appsByDestination.append(appByDest)
     # IP Address
     if "ips" in o365Id.keys():
         for iP in o365Id['ips']:
             if ":" in iP:
                 () # Skip ipv6
             else:
-                app = {
+                appByDest= {
                     "id": o365Id['id'],
                     "category": o365Id['category'],
                     "name": o365Id['serviceAreaDisplayName'],
@@ -71,16 +73,92 @@ for o365Id in o365Source:
                     "notes": None
                 }
             if "tcpPorts" in o365Id.keys():
-                app['tcpPorts'] = o365Id['tcpPorts']
+                appByDest['tcpPorts'] = o365Id['tcpPorts']
             if "udpPorts" in o365Id.keys():
-                app['udpPorts'] = o365Id['udpPorts']
+                appByDest['udpPorts'] = o365Id['udpPorts']
             if "notes" in o365Id.keys():
-                app['notes'] = o365Id['notes']
-            apps.append(app)
+                appByDest['notes'] = o365Id['notes']
+            appsByDestination.append(appByDest)
+
+
+# Define an array of the appnames to consolidate
+appsNameList = []
+for o365DisplayName in o365Source:
+    if o365DisplayName['serviceAreaDisplayName'] in appsNameList:
+        ()
+    else:
+        appsNameList.append(o365DisplayName['serviceAreaDisplayName'])
+
+# Parse out the appnames, and consolidate destinations and ports
+appsByName = []
+for appName in appsNameList:
+    appByName = {
+        "ids": [], # numerical value to all MS Apps in the source.  For tracking only
+        "categorys": [], # Microsoft given category - Optimzed, Rquired/Default, Allow
+        "name": appName, # Detailed display name
+        "destIP": [],
+        "destUrl": [],
+        "tcpPorts": [],
+        "udpPorts": [],
+        "isIcmp": True, # Default value
+        "czName": "Default Connector Zone", # Default value
+        "czId": None, # applicable only in API push
+        "notes": [],  # for human readability
+        "tags": "MS 365 All Apps" # Default tag name
+    }
+
+    for o365Name in o365Source:
+        if appName in o365Name["serviceAreaDisplayName"]:
+
+            # Append common traits
+            appByName["ids"].append(o365Name["id"])
+            appByName["categorys"].append(o365Name["category"])
+
+            if "notes" in o365Name.keys():
+                appByName['notes'].append(o365Name['notes'])
+            if "ips" in o365Name.keys():
+                appByName['destIP'].append(o365Name["ips"])
+            if "urls" in o365Name.keys():
+                appByName['destUrl'].append(o365Name["urls"])
+
+            # Concatenate all unique tcp ports
+            if "tcpPorts" in o365Name.keys():
+                tcpPortList = o365Name['tcpPorts'].split(',')
+                for tcpPort in tcpPortList:
+
+                    if (tcpPort+":tcp")in appByName['tcpPorts']:
+                        ()
+                    else:
+                        appByName['tcpPorts'].append(tcpPort +":tcp")
+
+            # Concatenate all unique udp ports
+            if "udpPorts" in o365Name.keys():
+                udpPortList = o365Name['udpPorts'].split(',')
+                for udpPort in udpPortList:
+
+                    if (udpPort+":udp") in appByName['udpPorts']:
+                        ()
+                    else:
+                        appByName['udpPorts'].append(udpPort +":udp")
+
+                    # Teams guidence recomends adding tcp as well.
+                    if (udpPort+":tcp") in appByName['tcpPorts']:
+                        ()
+                    else:
+                        appByName['tcpPorts'].append(udpPort +":tcp")
+
+
+    appsByName.append(appByName)
+# Dump output to the sample file
+with open(outputFileJS, 'w', encoding='utf-8') as fh:
+    #Write to file
+    json.dump(appsByName, fh, ensure_ascii=False, indent=4)
+
+
 
 
 # Dump to JSON for API automation
-apiBodyTemplate = {
+axisBodyTemplate = {
 "name":None,
 "type": "NetworkRange",
 "enabled": True,
@@ -94,9 +172,28 @@ apiBodyTemplate = {
     },
 "connectorZoneID": None
 }
-# Dump to CSV for parsing
-fields = apps[0].keys()
-with open(csvParseFile, 'w') as csvFh:
+
+# Axis bulk import axisBodyTemplate
+importFields = []
+with open(inputTemplateCSV, 'r') as importFh:
+    reader = csv.reader(importFh, delimiter = ',')
+    for csvRow in reader:
+        importFields.append(csvRow)
+# Build out the consolidated template by app display name match.
+for field in importFields:
+    ()
+
+
+
+
+# Dump to CSV based on destination.  For Human readability of data
+fields = appsByDestination[0].keys()
+with open(outputAppsByDestination, 'w') as csvFh:
     writer = csv.DictWriter(csvFh, fieldnames=fields)
     writer.writeheader()
-    writer.writerows(apps)
+    writer.writerows(appsByDestination)
+
+
+
+
+# Convert CSV to bulkImport
